8f20402d84612fe614cdcadb6ca10314ff10e271
hugo borini <hugoborini>
2020-05-13 15:11:38 +0200

    {add} todo

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,107 @@
-$body = document.querySelector("body");
+document.addEventListener("DOMContentLoaded", function(){
+	
+	class Model {
+		
+		constructor() {
+			this.pages = [
+				{
+					title: 'Homepage',
+                    url: '/',
+                    text: "HELLO WORLD",
+                    background: 'red'
+				},
+				{
+					title: 'Contact',
+					url: '/contact',
+					background: 'green'
+                },
+			]
+		}
 
-const printf = message => message;
+		getPageByUrl(url) {
+			return this.pages.find(page => page.url == url)
+		}
 
-const helloWord = printf("Hello word");
+	}
 
-const title = `<div class="title-container"><h1 class="title">${helloWord}</h1></div>`;
+	class View {
 
-$body.innerHTML += title;
+		constructor(elem) {
+			this.container = document.querySelector(elem)
+		}
+
+		changeTitle(text) {
+			this.container.innerHTML += text
+		}
+
+		changeBackground(color) {
+			this.container.style.background = color
+        }
+        centerFlex(){
+            this.container.style.display = "flex"
+            this.container.style.justifyContent = "center"
+        }
+        setFont(size){
+            this.container.style.fontSize = size
+        }
+        setFontFamily(fontFamily){
+            this.container.style.fontFamily = fontFamily
+        }
+        setFontWeight(fontWeight){
+            this.container.style.fontWeight = fontWeight
+        }
+        setMarginTop(marginTop){
+            this.container.style.marginTop = marginTop
+        }
+        setAnimation(anim){
+            this.container.style.animation = anim
+        }
+        getInputValue(elem){
+            return this.container.querySelector(elem).value
+        }
+        setFlexCollumCenter(){
+            this.container.style.display= 'flex'
+            this.container.style.flexDirection = "column"
+            this.container.style.alignItems= "center"
+        }
+        setInputValue(value){
+            this.container.querySelector(elem).value = value
+        }
+
+
+
+	}
+
+	function controller() {
+		let data = new Model()
+        const currentPage = data.getPageByUrl('/')
+
+        let input = new View(".input-todo");
+        let submit = new View(".input-todo");
+        let containerTodo = new View(".container-todo")
+
+        //INPUT 
+        input.changeTitle("<input class='txt' type=text>")
+        input.centerFlex()
+        input.setMarginTop("20px")
+
+        //Submit
+
+        submit.changeTitle("<input class='submit' type=submit>")
+
+        //container todo
+        containerTodo.setFlexCollumCenter()
+        containerTodo.setMarginTop("10px")
+
+        //elemTodo
+
+        document.querySelector(".submit").addEventListener("click", ()=>{
+            containerTodo.changeTitle(`<div class='item'>${input.getInputValue('.txt')} </div>`)
+            
+        });
+
+    }
+
+	controller()
+
+})
